447800a37b87bb460ad8d0b6a76fbdcf622ae9e7
vegetayu <wushenggoo@gmail.com>
2018-05-23 22:45:22 +0800

    save

--- app/src/main/java/com/capsule/exlc/MainActivity.java
+++ app/src/main/java/com/capsule/exlc/MainActivity.java
@@ -6,84 +6,53 @@ import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
 
 import capsule.bamboo.Logger;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends LifecycleLogActivity {
 
     private AlertDialog alertDialog;
+
+    Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Logger.v("vegeta", "onCreate");
 
-        alertDialog = new AlertDialog.Builder(this)
-                .setTitle("hello")
-                .setNegativeButton("cancel", new DialogInterface.OnClickListener() {
+        alertDialog = new AlertDialog.Builder(this).setTitle("hello").setNegativeButton("cancel",
+                new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
                         dialog.dismiss();
                     }
-                })
-                .setPositiveButton("ok", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        dialog.dismiss();
-                    }
-                })
-                .create();
+                }).setPositiveButton("ok", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        }).create();
 
-        findViewById(R.id.btnStart).setOnClickListener(new View.OnClickListener() {
+        btn = findViewById(R.id.btnStart);
+        btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(MainActivity.this, SecondActivity.class));
             }
         });
 
+//        btn.postDelayed(new Runnable() {
+//            @Override
+//            public void run() {
+//                if (!alertDialog.isShowing()) {
+//                    alertDialog.show();
+//                }
+//            }
+//        }, 3000);
+
     }
 
-    @Override
-    protected void onStart() {
-        super.onStart();
-        Logger.v("vegeta", "onStart");
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        Logger.v("vegeta", "onStop");
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        Logger.v("vegeta", "onDestroy");
-    }
-
-    @Override
-    protected void onRestart() {
-        super.onRestart();
-        Logger.v("vegeta", "onRestart");
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        Logger.v("vegeta", "onPause");
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        Logger.v("vegeta", "onResume");
-
-        if (!alertDialog.isShowing()) {
-//            alertDialog.show();
-
-        }
-    }
 
 
 }
--- app/src/main/java/com/capsule/exlc/SecondActivity.java
+++ app/src/main/java/com/capsule/exlc/SecondActivity.java
@@ -1,60 +1,26 @@
 package com.capsule.exlc;
 
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 
 import capsule.bamboo.Logger;
 
 
-public class SecondActivity extends AppCompatActivity {
+public class SecondActivity extends LifecycleLogActivity {
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
-        Logger.v("vegeta", "onCreate");
 
-//        Intent intent = new Intent(this, MainActivity.class);
-//        intent.setAction("helloo");
-//        intent.addCategory("");
-//        intent.setDataAndNormalize()
+        Fragment fragment = AFragment.newInstance();
+        getSupportFragmentManager().beginTransaction()
+                                   .add(R.id.fragment_container, fragment, "fragmentA")
+                                   .addToBackStack("fragmentA")
+                                   .commit();
 
     }
 
-    @Override
-    protected void onStart() {
-        super.onStart();
-        Logger.v("vegeta", "onStart");
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        Logger.v("vegeta", "onStop");
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        Logger.v("vegeta", "onDestroy");
-    }
-
-    @Override
-    protected void onRestart() {
-        super.onRestart();
-        Logger.v("vegeta", "onRestart");
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        Logger.v("vegeta", "onPause");
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        Logger.v("vegeta", "onResume");
-    }
 }
